4b6854b293726d41f00fa86680b2426a17438155
Shano1993 <stefan.hanotiau@gmail.com>
2021-11-15 16:34:30 +0100

    js

---
+++ exo 1/app.js
@@ -0,0 +1,29 @@
+
+let item = document.getElementsByTagName('li');
+console.log(item);
+
+item[0].style.listStyleType = "circle";
+item[0].innerText = "blabla"
+
+item[1].style.listStyleType = "circle";
+item[1].innerText = "blabla"
+
+item[2].style.listStyleType = "circle";
+item[2].innerText = "blabla"
+
+item[3].style.listStyleType = "circle";
+item[3].innerText = "blabla"
+
+item[4].style.listStyleType = "circle";
+item[4].innerText = "blabla"
+
+item[5].style.listStyleType = "circle";
+item[5].innerText = "blabla"
+
+
+item[6].style.listStyleType = "circle";
+item[6].innerText = "blabla"
+
+item[7].style.listStyleType = "circle";
+item[7].innerText = "blabla"
+
